144a3c5230558f7a61bd7d84409b9b73e2a6f568
slambuilder <slambuilder@users.noreply.github.com>
2016-09-22 19:56:28 -0700

    New project from ATMEL template.

---
+++ Bldecent-mcu/main.c
@@ -0,0 +1,13 @@
+// Copyright (C) 2016 SlamBuilder LLC. All rights reserved.
+#include "sam.h"
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}

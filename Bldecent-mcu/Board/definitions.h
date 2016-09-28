f9471fe24d640287cc90c5c2478bc95f29e5f6d3
slambuilder <slambuilder@users.noreply.github.com>
2016-09-27 20:17:48 -0700

    Configure clocks. CPU is 48MHz. Sample toggles PA02 GPIO pin.

---
+++ Bldecent-mcu/Board/definitions.h
@@ -0,0 +1,6 @@
+#ifndef BOARD_DEFS_H_
+#define BOARD_DEFS_H_
+
+#define CPU_FREQUENCY					(48000000ul)
+
+#endif /* BOARD_DEFS_H_ */
---
+++ Bldecent-mcu/Board/pins.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2016 SlamBuilder LLC. All rights reserved.
+
+#ifndef _BOARD_PINS_H_
+#define _BOARD_PINS_H_
+
+// DAC on PA02
+#define BOARD_PINS_DAC_PORT 0
+#define BOARD_PINS_DAC_PIN 2
+
+#endif // _BOARD_PINS_H_
